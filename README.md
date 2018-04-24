a062420acab458e66555941ddccbb6c8af98d157
Nathan Collard <n.w.j.collard@gmail.com>
2018-04-24 22:42:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Basic example of using docker to host the grpc example for C# behind an NGINX load balancer.
+
+To run:
+```
+dotnet build
+dotnet publish
+docker-compose build
+docker-compose up --scale app=4 -d
+dotnet run --project GreeterClient
+```
---
+++ csharp/GreeterServer/Program.cs
@@ -0,0 +1,58 @@
+// Copyright 2015 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Helloworld;
+
+namespace GreeterServer
+{
+    class GreeterImpl : Greeter.GreeterBase
+    {
+        // Server side handler of the SayHello RPC
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = $"Hello {request.Name}. {Environment.MachineName}"  });
+        }
+
+        // Server side handler for the SayHelloAgain RPC
+        public override Task<HelloReply> SayHelloAgain(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = $"Hello again {request.Name}. {Environment.MachineName}" });
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Greeter.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("0.0.0.0", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            while (true) Thread.Sleep(5000);
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
